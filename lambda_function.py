32c90db97a1bdd06cda19ccc3014e3eb7fabd248
anindaghosh <aninda.ghosh99@gmail.com>
2025-05-07 22:27:53 -0400

    Changes: Service working as expected

--- lambda_function.py
+++ lambda_function.py
@@ -1,19 +1,65 @@
 import json
+from supabase_client import get_supabase
+from utils import get_files_by_project, update_scan_status, scan_files
+
+supabase = get_supabase()
 
 
 def lambda_handler(event, context):
     # Get message from the queue event
     # For example, if using SQS, the message would be in event['Records'][0]['body']
-    # Here we just simulate a message
     message = event.get("Records", [{}])[0].get("body", {})
 
     if "scan_id" not in message:
         return {"statusCode": 400, "body": json.dumps("Missing scan_id in message")}
 
+    if "project_id" not in message:
+        return {"statusCode": 400, "body": json.dumps("Missing project_id in message")}
+
     # Process the message
     scan_id = message["scan_id"]
+    project_id = message["project_id"]
     print(f"Received Scan ID: {scan_id}")
-    return {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}
+    print(f"Received Project ID: {project_id}")
+
+    # Check if scan is valid
+    scan = (
+        supabase.table("scans")
+        .select("*")
+        .eq("id", scan_id)
+        .eq("project_id", project_id)
+        .execute()
+    )
+
+    if not scan.data:
+        print(f"Scan not found for ID: {scan_id} and Project: {project_id}")
+        return {"statusCode": 404, "body": json.dumps("Scan not found")}
+
+    # Check if scan is already in progress
+    if scan.data[0]["status"] != 0:
+        print(f"Scan already processed for ID: {scan_id}")
+        return {"statusCode": 400, "body": json.dumps("Scan already processed")}
+
+    # Update scan status to in progress
+    update_scan_status(
+        scan_id=scan_id,
+        status=1,
+    )
+
+    files = get_files_by_project(project_id)
+
+    if not files:
+        update_scan_status(
+            scan_id=scan_id,
+            status=-1,
+        )
+        return {"statusCode": 404, "body": json.dumps("No files found")}
+
+    scan_files(scan_id=scan_id, project_id=project_id, files=files)
+
+    print(f"Scan completed for ID: {scan_id}")
+
+    return {"statusCode": 200, "body": json.dumps("Scan is completed!")}
 
 
 def test_lambda_handler():
@@ -22,7 +68,10 @@ def test_lambda_handler():
             {
                 "messageId": "11d6ee51-4cc7-4302-9e22-7cd8afdaadf5",
                 "receiptHandle": "AQEBBX8nesZEXmkhsmZeyIE8iQAMig7qw...",
-                "body": {"scan_id": "d237f234-93f0-4d56-933e-eac18fca98e5"},
+                "body": {
+                    "scan_id": "312f7035-42ff-4ff4-8c89-620562bf7e62",
+                    "project_id": "288732f8-56b7-473c-ba3f-b7194d3ff4d5",
+                },
                 "attributes": {
                     "ApproximateReceiveCount": "1",
                     "SentTimestamp": "1573251510774",
@@ -42,9 +91,6 @@ def test_lambda_handler():
     }
     context = {}
     response = lambda_handler(event, context)
-    # assert response["statusCode"] == 200
-    # assert response["body"] == json.dumps("Hello from Lambda!")
-    print("Test passed!")
     # Run the test
 
 
